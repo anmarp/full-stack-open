af9787178a6a0ef733364c0ae691754903b58413
anmarp <67588125+anmarp@users.noreply.github.com>
2020-09-30 14:39:00 +0300

    Add solution to exercise 4.5

--- part-4/blog-list/utils/list-helper.js
+++ part-4/blog-list/utils/list-helper.js
@@ -8,7 +8,14 @@ const totalLikes = (blogs) => {
     : blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((mostLiked, currentBlog) => currentBlog.likes > mostLiked.likes ? currentBlog : mostLiked, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
