be70a31488dbb50ded88394370115442d64b1d6d
anmarp <67588125+anmarp@users.noreply.github.com>
2020-10-04 12:01:25 +0300

    Add solution to exercise 4.10

--- part-4/blog-list/tests/blog-api.test.js
+++ part-4/blog-list/tests/blog-api.test.js
@@ -4,6 +4,7 @@ const helper = require('./test-helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const { initialBlogs } = require('./test-helper')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -13,7 +14,7 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('all blogs are returned and they are in JSON format', async () => {
+test('all blogs are returned in JSON format', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
@@ -26,6 +27,29 @@ test('all blogs are returned and they are in JSON format', async () => {
 test('unique identifier property is named id', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a new blog is successfully added to the database', async () => {
+  const blog = {
+    title: 'Plok',
+    author: 'P. Lokker',
+    url: 'https://www.kookle.com/',
+    likes: 2
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+
+    for (i = 0; i < Object.keys(blog).length; i++) {
+      expect(response.body[helper.initialBlogs.length])
+        .toHaveProperty(Object.keys(blog)[i], Object.values(blog)[i])
+    }
 })
 
 afterAll(() => {
