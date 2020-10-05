fc48f4a0f83252644443ad3aaf1ae1a59addc619
anmarp <67588125+anmarp@users.noreply.github.com>
2020-10-05 20:44:49 +0300

    Add solution to exercise 4.17

--- part-4/blog-list/controllers/blogs.js
+++ part-4/blog-list/controllers/blogs.js
@@ -1,15 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const blog = new Blog(body)
+
+  const users = await User.find({})
+  blog.user = users[0].id
+
   const savedBlog = await blog.save()
+  users[0].blogs = users[0].blogs.concat(savedBlog._id)
+  await users[0].save()
+
   response.json(savedBlog)
 })
 
