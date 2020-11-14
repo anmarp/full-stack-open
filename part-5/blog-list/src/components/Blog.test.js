6c2921d864f7b1f71d9241ce43b11bbc135ad8aa
anmarp <67588125+anmarp@users.noreply.github.com>
2020-11-14 18:44:12 +0200

    Add solution to exercise 5.13

---
+++ part-5/blog-list/src/components/Blog.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Blog from './Blog'
+
+describe('<Blog />', () => {
+  const blog = {
+    title: 'Blog',
+    author: 'B. Logger',
+    url: 'https://www.google.com/',
+    likes: 13,
+    user: {
+      username: 'user',
+      name: 'U. Ser',
+      password: 'p4ssw0rd',
+      blogs: []
+    }
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  test('title and author are rendered by default', () => {
+    expect(component.container).toHaveTextContent(blog.title)
+    expect(component.container).toHaveTextContent(blog.author)
+  })
+
+  test('other details are not rendered by default', () => {
+    expect(component.container).not.toHaveTextContent(blog.url)
+    expect(component.container).not.toHaveTextContent(blog.likes)
+    expect(component.container).not.toHaveTextContent(blog.user.name)
+  })
+})
