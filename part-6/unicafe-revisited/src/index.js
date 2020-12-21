94fe5dca86ac9893d467a0c9dbd4753d10d3c84a
anmarp <67588125+anmarp@users.noreply.github.com>
2020-12-21 11:38:57 +0200

    Refactor rating functions into one function

--- part-6/unicafe-revisited/src/index.js
+++ part-6/unicafe-revisited/src/index.js
@@ -6,40 +6,22 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const rate = (type) => {
     store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
-  const neutral = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-
-  const reset = () => {
-    store.dispatch({
-      type: 'ZERO'
+      type: type
     })
   }
 
   return (
     <div>
       <button
-        onClick={good}>good</button>
+        onClick={() => rate('GOOD')}>good</button>
       <button
-        onClick={neutral}>neutral</button>
+        onClick={() => rate('OK')}>neutral</button>
       <button
-        onClick={bad}>bad</button>
+        onClick={() => rate('BAD')}>bad</button>
       <button
-        onClick={reset}>reset stats</button>
+        onClick={() => rate('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
       <div>neutral {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
